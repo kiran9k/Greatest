53df889f166eddf56d011a47d1d461dbd49e1ada
kiran9k <kiran9k99@gmail.com>
2013-06-14 09:45:26 +0530

    first commit

---
+++ greatest.py
@@ -0,0 +1,20 @@
+######################################################
+"""The program accepts 3 numbers from the user and then
+it checks the three to find the greatest among the three"""
+
+
+###################################################
+
+
+print "Please enter 3 numbers"
+a=input("\nEnter the first number:\t")
+b=input("\nEnter the seond number\t")
+c=input("\nEnter the third number\t")
+print ("\nThe numbers entered are: " +repr(a) +","+repr(b)+"," +repr(c)+"\n")
+no=a
+if no<b:
+    no=b
+if no<c:
+    no=c
+
+print (" greatest number is " +repr(no))
---
+++ readme.md
@@ -0,0 +1,2 @@
+This program is used to find the greatest of the 3 numbers.
+It accepts 3 numbers from the user and displays the greatest of them
