3090ecaaec1a390021330c72585f5b3c656d201f
kiran9k <kiran9k99@gmail.com>
2013-06-14 12:30:13 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -5,16 +5,16 @@ it checks the three to find the greatest among the three"""
 
 ###################################################
 
-
-print "Please enter 3 numbers"
-a=input("\nEnter the first number:\t")
-b=input("\nEnter the seond number\t")
-c=input("\nEnter the third number\t")
-print ("\nThe numbers entered are: " +repr(a) +","+repr(b)+"," +repr(c)+"\n")
-no=a
-if no<b:
-    no=b
-if no<c:
-    no=c
-
-print (" greatest number is " +repr(no))
+#!/usr/bin/python
+if __name__ =='__main__':
+    print "Please enter 3 numbers"
+    a=input("\nEnter the first number:\t")
+    b=input("\nEnter the seond number\t")
+    c=input("\nEnter the third number\t")
+    print ("\nThe numbers entered are: " +repr(a) +","+repr(b)+"," +repr(c)+"\n")
+    no=a
+    if no<b:
+        no=b
+    if no<c:
+        no=c
+    print (" greatest number is " +repr(no))
